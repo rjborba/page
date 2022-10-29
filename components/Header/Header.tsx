5e2daca7dc3a23f7f8469fb71fd40ed18b33d8ab
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2022-10-29 18:34:14 -0300

    Image update and base for Applications list

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,70 +1,109 @@
-import { Box, Grid, IconButton, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 import { FC } from "react";
-import BorbaImage from "../../images/borba.png";
+import BorbaImage1 from "../../images/borba1.jpg";
+// import BorbaImage2 from "../../images/borba2.jpg";
+// import BorbaImage3 from "../../images/borba3.jpg";
+// import BorbaImage4 from "../../images/borba4.jpg";
+// import BorbaImage5 from "../../images/borba5.jpg";
+// import BorbaImage6 from "../../images/borba6.jpg";
+// import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 import { SocialMediaRow } from "./SocialMediaRow";
 
 export const Header: FC = () => {
+  // const [borbaImage, setBorbaImage] = useState<StaticImageData>(BorbaImage1);
+  // const countRef = useRef(0);
+
+  // const borbaImagesArray = [
+  //   BorbaImage1,
+  //   BorbaImage2,
+  //   BorbaImage3,
+  //   BorbaImage4,
+  //   BorbaImage5,
+  // ];
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const imageIndex = (countRef.current + 1) % borbaImagesArray.length;
+  //     setBorbaImage(borbaImagesArray[imageIndex]);
+  //     countRef.current++;
+  //   }, 1000);
+
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+
   return (
-    <Box
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        height: "100vh",
-        transition: "0.15s",
-      }}
-    >
+    <>
       <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection={{ xs: "column-reverse", md: "row" }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          // height: "calc(100vh - 120px)",
+          height: "100vh",
+          transition: "0.15s",
+        }}
       >
         <Box
-          width={{ xs: "100%", md: "494px" }}
-          maxWidth={{ xs: "500px", md: "unset" }}
-          paddingTop={{ xs: "32px", md: "unset" }}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection={{ xs: "column-reverse", md: "row" }}
         >
-          <Typography fontSize={{ xs: 24, md: 32 }} fontWeight="bold">
-            Hello World, I am
-          </Typography>
-          <Typography
-            color="primary"
-            fontSize={{ xs: 36, md: 64 }}
-            fontWeight="bold"
-            whiteSpace={{ md: "nowrap" }}
-            lineHeight={"1"}
-            marginLeft={"-2px"}
-          >
-            Rodrigo Borba
-          </Typography>
-          <Typography fontSize={{ xs: 20, md: 36 }} fontWeight="light">
-            I code for web3, teach people, fly airplanes and play guitar
-          </Typography>
-          <Box width={{ xs: "300px", md: "400px" }}>
-            <SocialMediaRow />
-          </Box>
-        </Box>
-        <Box width={{ xs: "100%", md: "unset" }} flexGrow={1}>
           <Box
-            height={{ xs: "unset", md: "382px" }}
-            width={{ xs: "100%", md: "382px" }}
-            maxWidth={{ xs: 500, md: "unset" }}
-            border="12px solid white"
-            margin="auto"
+            width={{ xs: "100%", md: "494px" }}
+            maxWidth={{ xs: "100%", md: "unset" }}
+            paddingTop={{ xs: "32px", md: "unset" }}
           >
-            <Image
-              src={BorbaImage}
-              layout="responsive"
-              objectFit="cover"
-              height="100%"
-              width="100%"
-            />
+            <Typography fontSize={{ xs: 24, md: 32 }} fontWeight="bold">
+              Hello World, I am
+            </Typography>
+            <Typography
+              color="primary"
+              fontSize={{ xs: 36, md: 64 }}
+              fontWeight="bold"
+              whiteSpace={{ md: "nowrap" }}
+              lineHeight={"1"}
+              marginLeft={"-2px"}
+            >
+              Rodrigo Borba
+            </Typography>
+            <Typography fontSize={{ xs: 20, md: 36 }} fontWeight="light">
+              I code for web3, teach people, fly airplanes and play guitar
+            </Typography>
+            <Box
+              width={{ xs: "100%", md: "400px" }}
+              maxWidth={{ xs: "300px", md: "400px" }}
+            >
+              <SocialMediaRow />
+            </Box>
+          </Box>
+          <Box width={{ xs: "100%", md: "unset" }} flexGrow={1}>
+            <Box
+              height={{ xs: "unset", md: "382px" }}
+              width={{ xs: "100%", md: "382px" }}
+              maxWidth={{ xs: "100%", md: "unset" }}
+              border="12px solid white"
+              margin="auto"
+            >
+              <Image
+                src={BorbaImage1}
+                layout="responsive"
+                objectFit="cover"
+                height="100%"
+                width="100%"
+              />
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+      {/* <Box height={120} textAlign="center">
+        <Typography fontSize={16}>Scroll for more</Typography>
+        <ArrowDownwardIcon fontSize="medium" />
+      </Box> */}
+    </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,61 @@
-import { Container } from "@mui/material";
+import { Box, Container, Grid, Stack, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { Header } from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <Container fixed>
+    <Container fixed sx={{ maxWidth: { sm: 600, md: 900, lg: 1200 } }}>
       <Header />
+      <section style={{ display: "none" }}>
+        <main>
+          <Stack alignItems={{ xs: "center", md: "unset" }}>
+            <Typography variant="h3">Applications</Typography>
+            <Grid container gap={12} mb={64} mt={16}>
+              <Grid item xs={12} md={3}>
+                {/* TODO: Extract to a component */}
+                <Box
+                  height="280px"
+                  minWidth="280px"
+                  maxWidth="280px"
+                  margin="auto"
+                  border="12px solid white"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{
+                    transition: "0.15s",
+                    "&:hover": { transform: "scale(1.05)", cursor: "pointer" },
+                  }}
+                >
+                  <Typography fontSize={24} textAlign="center">
+                    Image of PJ Brazilian Tax Calculator
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Box
+                  height="280px"
+                  minWidth="280px"
+                  maxWidth="280px"
+                  margin="auto"
+                  border="12px solid white"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{
+                    transition: "0.15s",
+                    "&:hover": { transform: "scale(1.05)", cursor: "pointer" },
+                  }}
+                >
+                  <Typography fontSize={24} textAlign="center">
+                    Image of PJ Brazilian Tax Calculator
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Stack>
+        </main>
+      </section>
     </Container>
   );
 };
